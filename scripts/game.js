edf3b01b0e878138449d2980271e7565918b2d6e
fykyby <iploritpl@gmail.com>
2022-05-06 19:29:21 +0200

    Progress?

--- scripts/game.js
+++ scripts/game.js
@@ -1,70 +1,67 @@
 import { drawGameboard, drawShipCell } from "./display.js";
 
-class Ship {
-    constructor(startPos, length, direction) {
-        this.startPos = startPos;
-        this.length = length;
-        this.direction = direction;
-        this.hitcount = 0;
-        this.isSunk = false;
-        this.id = Date.now().toString();
+function Ship(startPos, size, direction) {
+    this.startPos = startPos;
+    this.size = size;
+    this.direction = direction;
+    this.hitcount = 0;
+    this.isSunk = false;
+    this.id = Date.now().toString();
 
-        this.create();
-    }
-
-    create() {
-        for (let i = 0; i < this.length; i++) {
+    this.create = function() {
+        for (let i = 0; i < this.size; i++) {
             gameboard.grid[this.startPos + i].shipId = this.id;
             drawShipCell(this.startPos + i, this.id);
         }
     }
 
-    hit() {
+    this.hit = function() {
         this.hitcount++;
         console.log(this.hitcount);
-        if (hitcount === length) sink();
+        if (this.hitcount === size) this.sink();
     }
 
-    sink() {
-        isSunk = true;
+    this.sink = function() {
+        this.isSunk = true;
+        console.log('sunk');
+    }
+
+    this.create();
+}
+
+function GridCell(index) {
+    this.index = index;
+    this.shipId = null;
+    this.isHit = false;
+
+    this.hit = function() {
+        if (this.isHit) return;
+
+        this.isHit = true;
+        if(this.shipId) {
+            ships.find(ship => ship.id === this.shipId.toString()).hit();
+        }
     }
 }
 
-class GridCell {
-    constructor(index) {
-        this.index = index;
-        this.shipId = null;
-        this.hit = false;
-    }
-
-    hit() {
-        this.hit = true;
-    }
-}
-
-class Gameboard {
-    constructor(size) {
-        this.size = size;
-        this.grid = [];
-
-        this.create();
-    }
-
-    create() {
+function Gameboard(size) {
+    this.size = size;
+    this.grid = [];
+    
+    this.create = function() {
         for (let i = 0; i < this.size * this.size; i++) {
             this.grid.push(new GridCell(i));
         }
-
         drawGameboard(this.grid);
     }
-
-    hit(index) {
-        // this.grid[index].hit();
-        console.log(this.grid[index]);
+    
+    this.hit = function(index) {
+        this.grid[index].hit();
     }
+
+    this.create();
 }
 
 export const gameboard = new Gameboard(10);
 let ships = [];
 ships.push(new Ship(64, 4, 0));
-console.log(gameboard);
