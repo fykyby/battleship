7eb945a1ddaff5f6ed3d159fb66e1937d6c35076
fykyby <iploritpl@gmail.com>
2022-05-06 19:54:13 +0200

    Sink ships

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,12 @@
 import { drawGameboard, drawShipCell } from "./display.js";
 
-function Ship(startPos, size, direction) {
+function Ship(startPos, size, direction, id) {
     this.startPos = startPos;
     this.size = size;
     this.direction = direction;
     this.hitcount = 0;
     this.isSunk = false;
-    this.id = Date.now().toString();
+    this.id = id;
 
     this.create = function() {
         for (let i = 0; i < this.size; i++) {
@@ -17,13 +17,12 @@ function Ship(startPos, size, direction) {
 
     this.hit = function() {
         this.hitcount++;
-        console.log(this.hitcount);
         if (this.hitcount === size) this.sink();
     }
 
     this.sink = function() {
         this.isSunk = true;
-        console.log('sunk');
+        gameboard.areAllShipsSunk();
     }
 
     this.create();
@@ -38,6 +37,7 @@ function GridCell(index) {
         if (this.isHit) return;
 
         this.isHit = true;
+
         if(this.shipId) {
             ships.find(ship => ship.id === this.shipId.toString()).hit();
         }
@@ -59,9 +59,17 @@ function Gameboard(size) {
         this.grid[index].hit();
     }
 
+    this.areAllShipsSunk = function() {
+        let sunk = ships.every(ship => ship.isSunk);
+        if(sunk) {
+            console.log('all ships sunk');
+        }
+    }
+
     this.create();
 }
 
 export const gameboard = new Gameboard(10);
 let ships = [];
-ships.push(new Ship(64, 4, 0));
+ships.push(new Ship(64, 4, 0, Date.now().toString()));
+ships.push(new Ship(21, 4, 0, (Date.now() + 10).toString()));
--- scripts/input.js
+++ scripts/input.js
@@ -4,8 +4,13 @@ const gameboardElement = document.querySelector('.grid');
 
 gameboardElement.addEventListener('click', (e) => {
     if (!e.target.classList.contains('grid-cell')) return;
-    if (e.target.classList.contains('ship')) {
-        const index = parseInt(e.target.getAttribute('data-index'));
-        gameboard.hit(index);
+
+    const index = parseInt(e.target.getAttribute('data-index'));
+    gameboard.hit(index);
+
+    if (!e.target.classList.contains('ship')) {
+        e.target.classList.add('hit-blank');
+    } else {
+        e.target.classList.add('hit-ship');
     }
 });
